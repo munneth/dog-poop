09e72bf3f90a7cda17f7d984aadf78a4d0fb6251
munneth <mugill@ucdavis.edu>
2025-07-04 18:29:31 -0700

    export model

---
+++ run-DLC-live.py
@@ -0,0 +1,16 @@
+import deeplabcut
+
+# Use the path to your project config.yaml
+config_path = r"C:\Users\munne\Documents\DLC\poop_project-munneth-2025-06-25\config.yaml"
+
+# This will export using the best snapshot (or you can specify which snapshot)
+deeplabcut.export_model(config_path)
+
+
+
+
+
+
+
+
+
